89f9b3662b828a28d2eb1130ce68e1692bf7f3c2
kenza-amira <kenza.amira1.KA@gmail.com>
2020-11-26 20:03:23 +0000

    update

--- src/main/java/my/project/aqmaps/App.java
+++ src/main/java/my/project/aqmaps/App.java
@@ -8,12 +8,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Geometry;
+import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
 
 /**
@@ -24,6 +30,9 @@ public class App
 {
     public static void main( String[] args ) throws IOException, InterruptedException
     {
+    	// Starting point of our drone
+    	var start = Point.fromLngLat(Double.parseDouble(args[4]),Double.parseDouble(args[3]));
+    	System.out.println(start);
     	// We only need one HttpClient, shared between all HttpRequests
     	final HttpClient client = HttpClient.newHttpClient();
     	// Saving this string as it will be needed for multiple URL's.
@@ -82,6 +91,12 @@ public class App
          * the words folder in our web server to find this information and create a list of points for our map.
          */
         var sensorsLocation = new ArrayList<Point>();
+        var lng = new ArrayList<Double>();
+        var lat = new ArrayList<Double>();
+        sensorsLocation.add(start);
+        lng.add(start.longitude());
+        lat.add(start.latitude());
+        int length = 0;
         for (Sensor sensor: sensorList) {
         	//We changed the format of the location string to be able to fetch the data from the server
         	String location = sensor.getLocation().trim().replace(".", "/");
@@ -96,17 +111,88 @@ public class App
         	//of a JSON list. Once we have the coordinates we create a geoJson Point.
         	var details = new Gson().fromJson(responseL.body(), LocationDetails.class);
         	var loc = Point.fromLngLat(details.coordinates.lng, details.coordinates.lat);
-        	sensorsLocation.add(loc);       	
+        	lng.add(details.coordinates.lng);
+        	lat.add(details.coordinates.lat);
+        	sensorsLocation.add(loc);
+        	length += 1;
         }
-
         for (Point p : sensorsLocation) {
         	var feature = Feature.fromGeometry((Geometry)p);
         	features.add(feature);
         }
         features.addAll(noFly);
+
+        
+        double[][] dists = new double [length][length];
+        	for (int i = 0; i < length; i ++) {
+        		for (int j = 0; j < length; j ++) {
+        			dists[i][j] = euclid(lng.get(i),lat.get(i),lng.get(j), lat.get(j));
+        		}
+        	}
+        // greedy search algorithm
+        var visited = new ArrayList<Integer>();
+        var route = new ArrayList<Integer>();
+        int counter = 1;
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(0);
+        route.add(0);
+        while (!queue.isEmpty() && counter < length) {
+        		counter += 1;
+        		Integer i = queue.remove();
+        		//System.out.println(i);
+        		visited.add(i);
+        		double[] next = dists[i];
+            	for (Integer x: visited) {
+            		next[x] = 0.0;
+            	}
+            	double value = Double.MAX_VALUE;
+            	for (Double d: next) {
+            		value = (d == 0) ? value : Math.min(value, d);
+            	}
+            	System.out.println(value);
+            	int index = findIndex(next,value);
+            	//System.out.println(index);
+            	queue.add(index);
+            	visited.add(index);
+            	route.add(index);
+        }
+        var orderedSensors = new ArrayList<Point>();
+        for (Integer i: route) {
+        	orderedSensors.add(sensorsLocation.get(i));
+        }
+        System.out.println(sensorsLocation.size());
+        var ls = LineString.fromLngLats(orderedSensors);
+        features.add(Feature.fromGeometry((Geometry)ls));
         var collections = FeatureCollection.fromFeatures(features);
         System.out.println(collections.toJson());
-        
-        
     }
+    public static double euclid(double x1, double y1, double x2, double y2) {
+    		    return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    		}
+    public static int findIndex(double arr[], double value) 
+    { 
+  
+        // if array is Null 
+        if (arr == null) { 
+            return -1; 
+        } 
+  
+        // find length of array 
+        int len = arr.length; 
+        int i = 0; 
+  
+        // traverse in the array 
+        while (i < len) { 
+  
+            // if the i-th element is t 
+            // then return the index 
+            if (arr[i] == value) { 
+                return i; 
+            } 
+            else { 
+                i = i + 1; 
+            } 
+        } 
+        return -1; 
+    } 
 }
