ebafd88c242d080cb86dc9a8f5f8fe29cb4491db
kenza-amira <kenza.amira1.KA@gmail.com>
2020-12-03 20:16:09 +0000

    added comments

--- src/main/java/my/project/aqmaps/Helpers.java
+++ src/main/java/my/project/aqmaps/Helpers.java
@@ -4,28 +4,50 @@ import java.util.ArrayList;
 
 import com.mapbox.geojson.Point;
 
+/**
+ * Helpers Class description.
+ * 
+ * @author kenzaamira This class focuses on providing various helper functions
+ *         to avoid messy code. It has methods to compute Pythagorean distances,
+ *         to find the Index in an array, to generate a distance matrix, and to
+ *         reorder arrays.
+ *
+ */
 public class Helpers {
-	
+
+	/**
+	 * This method computes Pythagorean distances. Since for this coursework, we
+	 * assume that locations are like points on a plane instead of points on the
+	 * surface of a sphere.
+	 * 
+	 * @param x1 x-coordinate of the first point (double)
+	 * @param y1 y-coordinate of the first point (double)
+	 * @param x2 x-coordinate of the second point (double)
+	 * @param y2 y-coordinate of the second point (double)
+	 * @return The Pythagorean distance between the two points (double)
+	 */
 	public double euclid(double x1, double y1, double x2, double y2) {
 		return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 	}
-	
+
+	/**
+	 * This method allows us to find the first occurrence of a value in an array
+	 * 
+	 * @param arr   Array in which we look for the value (array of doubles)
+	 * @param value The value we are looking for (double)
+	 * @return Index of first occurrence of the value in the array (int)
+	 */
+
 	public int findIndex(double arr[], double value) {
 
-		// If array is Null
 		if (arr == null) {
 			return -1;
 		}
 
-		// find length of array
 		int len = arr.length;
 		int i = 0;
-
-		// traverse in the array
 		while (i < len) {
 
-			// if the i-th element is t
-			// then return the index
 			if (arr[i] == value) {
 				return i;
 			} else {
@@ -34,33 +56,18 @@ public class Helpers {
 		}
 		return -1;
 	}
-	
-	public int findIndex(ArrayList<Double> arr, double value) {
 
-		// If array is Null
-		if (arr == null) {
-			return -1;
-		}
-
-		// find length of array
-		int len = arr.size();
-		int i = 0;
-
-		// traverse in the array
-		while (i < len) {
-
-			// if the i-th element is t
-			// then return the index
-			if (arr.get(i) == value) {
-				return i;
-			} else {
-				i = i + 1;
-			}
-		}
-		return -1;
-	}
-	
-	public double[][] generateDistanceMatrix(ArrayList<Double> lng, ArrayList<Double> lat, int length){
+	/**
+	 * This method allows us to generate a Distance matrix between all points in our
+	 * list of Sensors.
+	 * 
+	 * @param lng    All longitudes of the sensors (double)
+	 * @param lat    All latitudes of the sensors (double)
+	 * @param length The length of the sensors list (int)
+	 * @return A length x length matrix of distances (double) e.g dists[0][2] is the
+	 *         distance between the 1st Sensor and the 3rd sensor.
+	 */
+	public double[][] generateDistanceMatrix(ArrayList<Double> lng, ArrayList<Double> lat, int length) {
 		double[][] dists = new double[length][length];
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < length; j++) {
@@ -69,11 +76,25 @@ public class Helpers {
 		}
 		return dists;
 	}
-	
-	public void reorderArrays (ArrayList<Integer> route, ArrayList<Point> sensorsLocation,
-ArrayList<String> batteries, ArrayList<String> readings, ArrayList<String> w3wAddress,
-ArrayList<Point> orderedSensors, ArrayList<String> orderedBatteries,
-ArrayList<String> orderedReadings, ArrayList<String>w3wOrdered) {
+
+	/**
+	 * This method fills arrays so that they contain the sensors in an ordered way.
+	 * 
+	 * @param route            This parameter gives the required ordering (Integer)
+	 * @param sensorsLocation  Original ordering of the sensors Locations (Point)
+	 * @param batteries        Original ordering of the battery data (String)
+	 * @param readings         Original ordering of the readings data (String)
+	 * @param w3wAddress       Original ordering of the what 3 words addresses
+	 *                         (String)
+	 * @param orderedSensors   New ordering of the sensors (Point)
+	 * @param orderedBatteries New ordering of the battery data (String)
+	 * @param orderedReadings  New ordering of the readings data (String)
+	 * @param w3wOrdered       New ordering of the what 3 words addresses (String)
+	 */
+
+	public void reorderArrays(ArrayList<Integer> route, ArrayList<Point> sensorsLocation, ArrayList<String> batteries,
+			ArrayList<String> readings, ArrayList<String> w3wAddress, ArrayList<Point> orderedSensors,
+			ArrayList<String> orderedBatteries, ArrayList<String> orderedReadings, ArrayList<String> w3wOrdered) {
 		for (Integer i : route) {
 			orderedSensors.add(sensorsLocation.get(i));
 			orderedReadings.add(readings.get(i));
--- src/main/java/my/project/aqmaps/ServerReader.java
+++ src/main/java/my/project/aqmaps/ServerReader.java
@@ -15,69 +15,90 @@ import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Polygon;
 
+/**
+ * Server Reader Class description.
+ * 
+ * @author kenzaamira This class focuses on reading data from the web server. It
+ *         contains the methods to get a list of sensors and a method to retried
+ *         no-fly zones. The ServerReader object takes as arguments: String
+ *         host: contains the port we want to connect to String month, day and
+ *         year: to access the relevant folder
+ *
+ */
+
 public class ServerReader {
 	private String host;
 	private String month;
 	private String day;
 	private String year;
+
+	/* We only need one HttpClient for all requests */
 	final HttpClient client = HttpClient.newHttpClient();
-	
+
 	public ServerReader(String host, String month, String day, String year) {
 		this.host = host;
 		this.month = month;
 		this.day = day;
 		this.year = year;
 	}
-	
+
 	/**
 	 * This part of the code fetches the information stored in the web server maps
-	 * folder. It uses the first 3 arguments (day, month, year) to get the sensors
-	 * that need to be visited on that date. It then uses Gson parsing to to
-	 * deserialise the JSON list into an object of type ArrayList<Sensor>. Since We
-	 * can’t get a class associated with ArrayList<Sensor> we get its Type using
-	 * Java’s Reflection API which is used to examine or modify methods, classes, or
-	 * interfaces at runtime. This allows use to have a Sensor Object (With
-	 * location, battery and reading) and to have a List of sensors to visit on that
-	 * date.
+	 * folder. It uses the 3 following arguments: day, month and year to get the
+	 * sensors that need to be visited on that date. It then uses Gson parsing to to
+	 * deserialise the JSON list into an object of type ArrayList of Sensor.
+	 * <p>
+	 * Since we can’t get a class associated with ArrayList of Sensors we get its
+	 * Type using Java’s Reflection API which is used to examine or modify methods,
+	 * classes, or interfaces at runtime. This allows use to have a Sensor Object
+	 * (With location, battery and reading).
 	 * 
+	 * @throws IOException if there's a problem with sending the request
+	 * @throws InterruptedException if there's a problem with sending the request
+	 * @return This method returns the list of sensors to visit on that date
 	 */
-	
-	public ArrayList<Sensor> getSensorList() throws IOException, InterruptedException{
+
+	public ArrayList<Sensor> getSensorList() throws IOException, InterruptedException {
 		String urlVisit = host + "/maps/" + year + "/" + month + "/" + day + "/air-quality-data.json";
 
-		var requestV = HttpRequest.newBuilder().uri(URI.create(urlVisit)).build();
-		var responseV = client.send(requestV, BodyHandlers.ofString());
+		var requestVisit = HttpRequest.newBuilder().uri(URI.create(urlVisit)).build();
+		var responseVisit = client.send(requestVisit, BodyHandlers.ofString());
 
-		Type listType = new TypeToken<ArrayList<Sensor>>() {}.getType();
-		ArrayList<Sensor> sensorList = new Gson().fromJson(responseV.body(), listType);
-		
+		Type listType = new TypeToken<ArrayList<Sensor>>() {
+		}.getType();
+		ArrayList<Sensor> sensorList = new Gson().fromJson(responseVisit.body(), listType);
+
 		return sensorList;
 	}
-	
+
 	/**
 	 * Here we use GeoJson parsing to retrieve data about the no fly zones. Like
 	 * before, we access information stored on the web server only this time it's in
 	 * the buildings folder. Since it is a geoJSON string. We are able to easily
-	 * retrieve the Feature Collection of our no fly zones. I've also created an
-	 * ArrayList of LineString to store the LineStrings that make our Polygon. This 
-	 * will be helpful later on for the path finding
+	 * retrieve the Feature Collection of our no fly zones.
+	 * 
+	 * @throws IOException if there's a problem with sending the request
+	 * @throws InterruptedException if there's a problem with sending the request
+	 * @return This method returns an ArrayList of type LineString that contains the
+	 *         outer parts of our polygons.
 	 */
-	
-	public ArrayList<LineString> getNoFlyZones() throws IOException, InterruptedException{
+
+	public ArrayList<LineString> getNoFlyZones() throws IOException, InterruptedException {
 		String urlDeadZone = host + "/buildings/no-fly-zones.geojson";
-		var requestD = HttpRequest.newBuilder().uri(URI.create(urlDeadZone)).build();
-		var responseD = client.send(requestD, BodyHandlers.ofString());
-		var obstacles = FeatureCollection.fromJson(responseD.body());
+
+		var requestDeadZone = HttpRequest.newBuilder().uri(URI.create(urlDeadZone)).build();
+		var responseDeadZone = client.send(requestDeadZone, BodyHandlers.ofString());
+		var obstacles = FeatureCollection.fromJson(responseDeadZone.body());
+
 		var noFly = obstacles.features();
-		
+
 		var allZones = new ArrayList<LineString>();
-		for (Feature f: noFly) {
+		for (Feature f : noFly) {
 			var geo = f.geometry();
-			var poly = (Polygon)geo;
-			allZones.add(poly.outer());
+			var polygone = (Polygon) geo;
+			allZones.add(polygone.outer());
 		}
 		return allZones;
 	}
 
-	
 }
